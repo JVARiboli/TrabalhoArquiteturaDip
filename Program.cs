4807af6c4f9e2cc39611e17ea25b9942378ad72d
João Vinicius Riboli <joaoviniciusriboli@gmail.com>
2025-06-12 20:30:32 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿enum State { On, Off }
+
+class Lamp
+{
+    State State;
+
+    public Lamp(State State)
+    {
+        this.State = State;
+    }
+
+    public void Operate()
+    {
+        State = State == State.On ? State.Off : State.On;
+        Console.WriteLine("Luz " + (State == State.On ? "Ligada" : "Desligada"));
+    }
+}
+
+class Switch
+{
+    private Lamp lamp;
+
+    public Switch(Lamp device)
+    {
+        this.lamp = device;
+    }
+
+    public void Press()
+    {
+        lamp.Operate();
+    }
+}
